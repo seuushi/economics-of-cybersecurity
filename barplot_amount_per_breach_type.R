566f0c4f96574204c23dc5dbbfb2b9fb1185ae86
seuushi <seuushi@users.noreply.github.com>
2017-09-18 10:30:09 +0200

    Added the first draft of assignment block 2 + two barplots

---
+++ barplot_amount_per_breach_type.R
@@ -0,0 +1,8 @@
+tob <- Privacy_Rights_Clearinghouse_Data_Breaches_Export$`Type of breach`
+tob_table <- table(tob)
+t_tob_table <- t(tob_table)
+barplot(t_tob_table[1,], main="Amount of breaches per breach type", xlab="Type of breach", ylab="Amount", ylim=c(0, 1000), cex.names=0.8)
+
+# Add text at top of bars
+xx <- barplot(t_tob_table[1,], main="Amount of breaches per breach type", xlab="Type of breach", ylab="Amount", ylim=c(0, 1000), cex.names=0.8)
+text(x=xx, y=t_tob_table[1,], label=t_tob_table[1,], pos=3, cex=0.8)
